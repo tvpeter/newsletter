c96bf36466837cad9b242affe97ccce3ade14ae5
Vihiga Tyonum <withtvpeter@gmail.com>
2024-08-23 16:33:48 +0100

    add spawning app and binding to random address

---
+++ tests/health_check.rs
@@ -0,0 +1,27 @@
+use std::net::TcpListener;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind a random port");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    let address = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(200, response.status().as_u16());
+    assert_eq!(Some(0), response.content_length());
+}
