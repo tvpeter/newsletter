e9f4c8332dec7c05c04a5db7b3b2d3951636dec1
Vihiga Tyonum <withtvpeter@gmail.com>
2024-08-26 03:29:57 +0100

    add conf crate and reading configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::net::TcpListener;
-use zero2prod::startup::run;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind a random port");
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("failed to read configuration");
+
+    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
 
     run(listener)?.await
 }
