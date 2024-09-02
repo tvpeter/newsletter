eaa9868a9d743a708a7660da0cc686bacb3f744f
Vihiga Tyonum <withtvpeter@gmail.com>
2024-09-02 09:37:24 +0100

    add tracing instrumentation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -8,17 +9,18 @@ pub struct FormData {
     email: String,
     name: String,
 }
-
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name= %form.name
+        )
+        )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "adding new subscriber",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    );
-    let _request_span_guard = request_span.enter();
-    tracing::info_span!("saving new subscriber details to database");
+    let query_span = tracing::info_span!("saving new subscriber details to database");
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -30,15 +32,10 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now()
     )
     .execute(pool.as_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            tracing::info_span!(
-                "request {}: new subscriber details have been saved",
-                %request_id
-            );
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
             tracing::error!("request {}: Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
